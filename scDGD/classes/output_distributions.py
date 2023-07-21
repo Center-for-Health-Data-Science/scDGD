c409e0c2171fc730ad0eb1ebb3358b149041b975
viktoriaschuster <viki.schuster@web.de>
2023-07-21 18:06:12 +0200

    add new count modelling option (probability)

--- scDGD/classes/output_distributions.py
+++ scDGD/classes/output_distributions.py
@@ -25,36 +25,45 @@ class NBLayer(nn.Module):
     A negative binomial of scaled values of m and learned parameters for r.
     mhat = m/M, where M is the scaling factor
 
-    The scaled value mhat is typically the output value from the NN
+    The scaled value mhat is typically the output value from the NN, 
+    but we are expanding to the option of modelling the probability.
 
     If rhat=None, it is assumed that the last half of mhat contains rhat.
 
     m = M*mhat
     '''
-    def __init__(self, out_dim, r_init, scaling_type='max', reduction='none'):
+    def __init__(self, out_dim, r_init, scaling_type='max', output='mean', activation='sigmoid', reduction='none'):
         super(NBLayer, self).__init__()
 
         # initialize parameter for r
         # real-valued positive parameters are usually used as their log equivalent
         self.log_r = torch.nn.Parameter(torch.full(fill_value=math.log(r_init-1), size=(1,out_dim)), requires_grad=True)
-        # determine the type of activation based on scaling type
-        if scaling_type == 'max':
-            self.activation = 'sigmoid'
-        elif scaling_type == 'total_count':
-            self.activation = 'softmax'
-        elif scaling_type in ['mean','median']:
-            self.activation = 'softplus'
-        else:
-            raise ValueError('Unknown scaling type specified. Please use one of: "library", "total_count", "mean", or "median".')
+        self.scaling_type = scaling_type
+        self.output = output
+        self.activation = activation
         self.reduction = reduction
+
+        #self.activ_layer = nn.ModuleList()
+        if self.activation == 'sigmoid':
+            self.activ_layer = nn.Sigmoid()
+        elif self.activation == 'softmax':
+            self.activ_layer = nn.Softmax(dim=-1)
+        elif self.activation == 'softplus':
+            self.activ_layer = nn.Softplus()
+        else:
+            raise ValueError('Activation function not recognized')
+    
+    @property
+    def r(self):
+        return torch.exp(self.log_r)+1
     
     def forward(self, x):
-        if self.activation == 'sigmoid':
-            return torch.sigmoid(x)
-        elif self.activation == 'softmax':
-            return torch.softmax(x, dim=-1)
-        else:
-            return F.softplus(x)
+        if self.output == 'mean':
+            return self.activ_layer(x)
+        elif self.output == 'prob':
+            p = self.activ_layer(x)
+            mean = self.r * (1-p)/p
+            return mean
 
     # Convert to m from scaled variables
     def rescale(self,M,mhat):
@@ -65,9 +74,9 @@ class NBLayer(nn.Module):
         # mhat has dim (nbatch,dim)
         # r has dim (1,dim)
         if gene_id is not None:
-            loss = -logNBdensity(x,self.rescale(M,mhat),(torch.exp(self.log_r)+1)[0,gene_id])
+            loss = -logNBdensity(x,self.rescale(M,mhat), self.r[0,gene_id])
         else:
-            loss = -logNBdensity(x,self.rescale(M,mhat),(torch.exp(self.log_r)+1))
+            loss = -logNBdensity(x,self.rescale(M,mhat), self.r)
         if self.reduction == 'none':
             return loss
         elif self.reduction == 'sum':
@@ -75,12 +84,13 @@ class NBLayer(nn.Module):
 
     # The logprob of the tensor
     def logprob(self,x,M,mhat):
-        return logNBdensity(x,self.rescale(M,mhat),(torch.exp(self.log_r)+1))
+        return logNBdensity(x,self.rescale(M,mhat), self.r)
 
     def sample(self,nsample,M,mhat):
         # Note that torch.distributions.NegativeBinomial returns FLOAT and not int
         with torch.no_grad():
             m = self.rescale(M,mhat)
-            probs = m/(m+torch.exp(self.log_r))
-            nb = torch.distributions.NegativeBinomial((torch.exp(self.log_r)+1), probs=probs)
+            #probs = m/(m+torch.exp(self.log_r))
+            probs = self.r / (m + self.r)
+            nb = torch.distributions.NegativeBinomial(self.r, probs=probs)
             return nb.sample([nsample]).squeeze()
